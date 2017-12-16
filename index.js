d237ef1e9d471ed7538696728ea798222f8e917c
AliBaig97 <alibaig97@hotmail.com>
2017-12-15 20:31:53 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on("ready", function(){
+  console.log("Ready");
+});
+
+const prefix = "_";
+
+const randomWord = require("random-word");
+
+var interval; //a random word every 5 or 10 seconds?
+var seconds = 8;
+var wrongServer = false;
+
+client.on("message", function(message){
+  if(message.author.equals(client.user)) return;
+  if(message.guild.name !== 'Elo Geeks'){
+    console.log('works!');
+    wrongServer = true;
+    return;
+  }
+
+  if(message.content.startsWith(prefix + "words") && !wrongServer){
+    message.channel.send("... Starting Random Word Generator. Have Fun!");
+
+    message.channel.send(randomWord()); //first randomWord before the interval
+    interval = setInterval(function(){
+      message.channel.send(randomWord());
+    }, seconds * 1000);
+  }
+
+  if(message.content.startsWith(prefix + "end")){ //stop the words
+    console.log("Ending words");
+    clearInterval(interval);
+    message.channel.send("..Ending Random Word Generator. Goodbye!");
+  }
+
+  if(message.content.startsWith(prefix + "settings")){
+    var settingsOutput = "---------------\n"
+                       + "THE OPTIONS\n"
+                       + "---------------\n"
+                       + "_change seconds - Adjust the speed of the words spitting out\n"
+                       + "--------------- \n";
+
+    message.channel.send(settingsOutput);
+  }
+  
+  if(message.content.startsWith(prefix + "help")){ //_help info
+    var helpOutput = "to be implemented";
+
+    message.channel.send(helpOutput);
+  }
+
+});
+
+client.login(process.env.BOT_TOKEN);
